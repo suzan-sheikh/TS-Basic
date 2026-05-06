b41ca81c4da4f9b9f78a96dea6f91724a2ab6474
suzan-sheikh <mdsuzanskh@gmail.com>
2026-05-06 10:03:32 +0600

    primitive type use

---
+++ ts_basic.ts
@@ -0,0 +1,16 @@
+//* Primitive Type => TypeScript is support total 7 type of data like:
+/*
+1.  string => "string";
+2.  number => 123;
+3.  boolean => tru/false;
+4.  null => null;
+5.  undefine => undefine;
+6.  symbol => !$%&*(;
+7.  Bigint => 125458795421 + 12546598712 = 23456897845131
+*/
+//? Example:
+
+let Myname: string = "suzan";
+let age: number = 1235;
+let isActive: boolean = true;
+
